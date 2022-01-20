1ac102add30039f0e41700cbe883dfbf2513fc58
Aman Khandelwal <aman.khandelwal1205@gmail.com>
2022-01-20 16:00:58 +0530

    bug fixes

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -56,10 +56,10 @@ const ProductCard = ({
 <a href="#_" className="mt-4 relative inline-flex items-center justify-start py-3 pl-4 pr-12 overflow-hidden font-semibold text-black text-sm transition-all duration-150 ease-in-out rounded hover:pl-10 hover:pr-6 bg-white group">
     <span className="absolute bottom-0 left-0 w-full h-1 transition-all duration-150 ease-in-out bg-yellow-400 group-hover:h-full"></span>
     <span className="absolute right-0 pr-4 duration-200 ease-out group-hover:translate-x-12">
-        <svg className="w-5 h-5 text-amazon_blue" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+        <svg className="w-5 h-5 text-amazon_blue" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
     </span>
     <span class="absolute left-0 pl-2.5 -translate-x-12 group-hover:translate-x-0 ease-out duration-200">
-        <svg className="w-5 h-5 text-amazon_blue" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+        <svg className="w-5 h-5 text-amazon_blue" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
     </span>
     <span class="relative w-full text-left transition-colors duration-200 ease-in-out group-hover:text-black">Add To Cart</span>
 </a>
