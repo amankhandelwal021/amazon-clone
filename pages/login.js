48df8183654bdf5fd8263f3b553306dd165cbd82
Aman Khandelwal <aman.khandelwal1205@gmail.com>
2022-01-20 17:51:01 +0530

    further changes

--- pages/login.js
+++ pages/login.js
@@ -21,26 +21,26 @@ const login = () => {
 
         <a
           href=""
-          class="my-5 relative inline-flex items-center px-12 py-3 overflow-hidden text-base font-semibold text-amazon_blue border-2 border-amazon_blue rounded-full hover:text-white group hover:bg-gray-50"
+          className="my-5 relative inline-flex items-center px-12 py-3 overflow-hidden text-base font-semibold text-amazon_blue border-2 border-amazon_blue rounded-full hover:text-white group hover:bg-gray-50"
         >
-          <span class="absolute left-0 block w-full h-0 transition-all bg-amazon_blue opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
-          <span class="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
+          <span className="absolute left-0 block w-full h-0 transition-all bg-amazon_blue opacity-100 group-hover:h-full top-1/2 group-hover:top-0 duration-400 ease"></span>
+          <span className="absolute right-0 flex items-center justify-start w-10 h-10 duration-300 transform translate-x-full group-hover:translate-x-0 ease">
             <svg
-              class="w-5 h-5"
+              className="w-5 h-5"
               fill="none"
               stroke="currentColor"
               viewBox="0 0 24 24"
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M14 5l7 7m0 0l-7 7m7-7H3"
               ></path>
             </svg>
           </span>
-          <span class="relative">Sign In With Google</span>
+          <span className="relative">Sign In With Google</span>
         </a>
       </div>
     </div>
